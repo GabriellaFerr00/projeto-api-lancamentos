b6179b3c2f3f434078865d58d56d687baacb28c2
GabriellaFerr00 <gabriellalok3@gmail.com>
2025-05-28 22:42:53 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/example/projeto_api_lancamentos/ProjetoApiLancamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_api_lancamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiLancamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiLancamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_api_lancamentos/ProjetoApiLancamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_api_lancamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiLancamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
