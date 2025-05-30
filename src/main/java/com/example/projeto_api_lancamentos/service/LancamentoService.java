fa5058737018eddb39aa5f60019ab89b90614b6a
GabriellaFerr00 <gabriellalok3@gmail.com>
2025-05-30 01:28:28 -0300

    criar endpoint de exibir os lancamentos

---
+++ src/main/java/com/example/projeto_api_lancamentos/service/LancamentoService.java
@@ -0,0 +1,42 @@
+package com.example.projeto_api_lancamentos.service;
+
+import com.example.projeto_api_lancamentos.handler.Messages;
+import com.example.projeto_api_lancamentos.handler.exception.RegrasBuscaRepositoryException;
+import com.example.projeto_api_lancamentos.handler.exception.RegrasDadosDeEntradaException;
+import com.example.projeto_api_lancamentos.model.LancamentoEntity;
+import com.example.projeto_api_lancamentos.model.dto.LancamentoResponseDTO;
+import com.example.projeto_api_lancamentos.repository.LancamentoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LancamentoService {
+    private final LancamentoRepository repository;
+
+
+    public List<LancamentoResponseDTO> buscarLancamentoPorCliente(Long idConta) {
+        if (idConta == null) {
+            throw new RegrasDadosDeEntradaException(Messages.ID_INCORRETO);
+        }
+
+        List<LancamentoEntity> lancamentos = repository.findByContaClienteId(idConta);
+
+        if (lancamentos.isEmpty()) {
+            throw new RegrasBuscaRepositoryException(Messages.CONTA_NAO_ENCONTRADA + idConta);
+        }
+
+        return lancamentos.stream()
+                .map(this::converteEntidadeParaDTO)
+                .toList();
+    }
+
+    private LancamentoResponseDTO converteEntidadeParaDTO(LancamentoEntity entidade) {
+        return new LancamentoResponseDTO(entidade.getTipoTransacaoType(), entidade.getValorTransacao(), entidade.getDataTransacao());
+    }
+
+}
